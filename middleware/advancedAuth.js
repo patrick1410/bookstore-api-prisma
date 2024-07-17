13c17c29d488dfe5cb2ce20627d82b0bfb5fea66
Patrick <patrick_btc_96@hotmail.com>
2024-07-17 18:23:10 +0200

    change: audience in advancedAuth

--- middleware/advancedAuth.js
+++ middleware/advancedAuth.js
@@ -1,6 +1,6 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 export const checkJwt = auth({
-  audience: "https://book-store-api", // e.g. https://book-store-api
-  issuerBaseURL: "https://dev-ezil5aozoabyhhcn.eu.auth0.com/",
+  audience: "https://bookstore-api-prisma",
+  issuerBaseURL: `https://dev-ezil5aozoabyhhcn.eu.auth0.com/`,
 });
